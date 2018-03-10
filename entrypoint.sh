17f3b582a36afe72e8f2d806fa599cc9345c5e0e
Stuart Johnson <stuart@logicethos.com>
2018-03-10 18:34:13 +0000

    Add FACTOMD to php env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 echo "env[RECAPTCHA] = $RECAPTCHA" >> /etc/php/7.0/fpm/pool.d/www.conf
 echo "env[RECAPTCHA_SECRET] = $RECAPTCHA_SECRET" >> /etc/php/7.0/fpm/pool.d/www.conf
+echo "env[FACTOMD] = $FACTOMD" >> /etc/php/7.0/fpm/pool.d/www.conf
 
 if [[ -v DOMAIN ]]; then
     /root/acme.sh --issue --standalone -d $DOMAIN --key-file /etc/ssl/private/nginx.key  --fullchain-file /etc/ssl/certs/nginx.crt
